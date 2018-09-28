5973f02989c10b61ec4a06fed2d9893fef45a84b
Przemyslaw Dabrowski <bluh@outlook.com>
2018-09-28 15:49:11 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -28,8 +28,10 @@ class ApplicationController < Sinatra::Base
 
   post '/sessions' do
     @user = User.find_by(email: params["email"], password: params["password"])
-    session[:id] = @user.id
-    redirect '/users/home'
+    if @user != nil
+      session[:id] = @user.id
+      redirect '/users/home'
+    end
   end
 
   get '/sessions/logout' do
